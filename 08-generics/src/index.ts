6533f9c4f3fb431d0a544913bc6d76dbff29e4e4
LP-React <ljamirp30@gmail.com>
2025-09-05 17:27:35 -0500

    end class-08

---
+++ 08-generics/src/index.ts
@@ -0,0 +1,6 @@
+import { getPokemon } from "./generics/get-pokemon";
+
+getPokemon(4)
+    .then(pokemon => console.log(pokemon.sprites.back_default))
+    .catch(error => console.error(error))
+    .finally(() => console.log('Fin de getPokemon'))
